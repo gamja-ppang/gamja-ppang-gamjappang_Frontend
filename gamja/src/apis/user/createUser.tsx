dc4f740a8ca193194a949414631c7ebaebc03995
taeyeon <greenjuni7@naver.com>
2026-03-26 08:40:31 +0900

    feat: 오류/예외상황 처리 로직 구현

--- gamja/src/apis/user/createUser.tsx
+++ gamja/src/apis/user/createUser.tsx
@@ -14,8 +14,12 @@ const CreateUser = async ({ username, email, password }: userCreate) => {
       password,
     });
     return response.data;
-  } catch (error) {
-    console.log(error);
+  } catch (error: any) {
+    const errorData = error.response?.data?.code;
+
+    if (errorData == "USR_409") alert("이미 가입된 메일입니다.");
+    if (errorData == "GLB_500") alert("서버 오류가 발생했습니다.");
+    if (errorData == "GLB_400") alert("잘못된 요청입니다.");
     throw error;
   }
 };
