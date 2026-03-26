32ed46762a4347deb7e5c99bedb3dc64243a3933
taeyeon <greenjuni7@naver.com>
2026-03-26 20:09:03 +0900

    feat: api인터렉션 추가 및 accessToken 추가

--- gamja/src/apis/index.tsx
+++ gamja/src/apis/index.tsx
@@ -12,11 +12,20 @@ export const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token") || null;
+    const token = localStorage.getItem("accessToken") || null;
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
   },
   (error) => {
     return Promise.reject(error);
