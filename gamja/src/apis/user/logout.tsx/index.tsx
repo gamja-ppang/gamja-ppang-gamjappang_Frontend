cbd1a630c5c6704d624f6034c784a04966fc5bf4
taeyeon <greenjuni7@naver.com>
2026-03-31 09:49:10 +0900

    feat: 로그아웃 기능 추가

--- gamja/src/apis/user/logout.tsx/index.tsx
+++ gamja/src/apis/user/logout.tsx/index.tsx
@@ -1,1 +1,21 @@
-export const Logout = () => {};
+import { api } from "../..";
+import type { type } from "./type";
+
+export const Logout = async ({ refreshToken }: type) => {
+  try {
+    const response: any = await api.post(`/api/v1/auth/logout`, {
+      refreshToken,
+    });
+
+    return response;
+  } catch (error: any) {
+    const errorData = error.response?.data?.description || "로그아웃 실패";
+    alert(errorData);
+    throw error;
+  } finally {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+
+    window.location.replace("./login");
+  }
+};
