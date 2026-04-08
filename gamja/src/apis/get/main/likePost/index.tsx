27343ba356f7b9ef8df85b43faec4e3db60dfa20
taeyeon <greenjuni7@naver.com>
2026-04-08 22:45:09 +0900

    feat: type 연결

--- gamja/src/apis/get/main/likePost/index.tsx
+++ gamja/src/apis/get/main/likePost/index.tsx
@@ -1,9 +1,10 @@
 import { api } from "../../..";
+import type { Type } from "./type";
 
 export const LikePost = async () => {
   try {
-    const response = await api.get(`/api/v1/me/liked-posts`);
-    
+    const response = await api.get<Type>(`/api/v1/me/liked-posts`);
+
     return response;
   } catch (error) {
     console.log(error);
