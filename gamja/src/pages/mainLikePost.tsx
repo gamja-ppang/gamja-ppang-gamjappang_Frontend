d896d9afdc35508bd9a07c1903ff1ee064c88c87
taeyeon <greenjuni7@naver.com>
2026-04-14 10:07:49 +0900

    feat: 좋아요 목록 화면에 표시

--- gamja/src/pages/mainLikePost.tsx
+++ gamja/src/pages/mainLikePost.tsx
@@ -1,14 +1,45 @@
 import styled from "styled-components";
-import Post from "../components/main/post";
 import MainPageLayouts from "../layouts/mainPageLoyouts";
+import { LikeComment } from "../components/user/likeComment";
+import type { Type } from "../apis/get/main/likePost/type";
+import { useEffect, useState } from "react";
+import { LikePost } from "../apis/get/main/likePost";
 
 const MainLikePost = () => {
+  const [posts, setPosts] = useState<Type[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const data = await LikePost();
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getPost();
+  }, []);
+
+  if (loading === true) return <div>로딩중</div>;
+
   return (
     <>
       <MainPageLayouts>
         <PostWrap>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <LikeComment
+                key={post.id}
+                name={post.author.name}
+                title={post.title}
+                likeCount={post.likeCount}
+                createdAt={post.createdAt}
+              />
+            );
+          })}
         </PostWrap>
       </MainPageLayouts>
     </>
